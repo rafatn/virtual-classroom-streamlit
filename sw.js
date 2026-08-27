2b411029d50b3f9e98f8b49071596d29da6b9c0e
rafatn <raftn4@gmail.com>
2026-08-27 03:37:11 +0300

    Implement service worker install and fetch listeners
    Add service worker event listeners for install and fetch events.

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker installed');
+});
 
+self.addEventListener('fetch', (e) => {
+  // מאפשר לאפליקציה לרוץ חלק
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
